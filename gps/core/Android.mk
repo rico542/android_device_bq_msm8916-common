d23d9cc1bfb030e2c3805a3e17b35e48eb010e61
Rico <rico@alwaysdata.net>
2018-04-15 07:01:32 +0200

    fix LocApiBase undefined call to logprint
    Change-Id: Id0071624ebae08b113d38bc244d5c795a58584bc

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -15,6 +15,8 @@ else ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 LOCAL_CFLAGS += -DPDK_FEATURE_SET
 endif
 
+LOCAL_LDLIBS := -ldl -llog
+
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
